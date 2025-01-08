5df0fc195ce85cf9e60e814c7656f851be278e87
Arman-Kabir <armankabir8@gmail.com>
2025-01-08 13:57:25 +0600

    todos service

--- first-ng-app/src/app/components/header/header.component.ts
+++ first-ng-app/src/app/components/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TodosService } from '../../services/todos.service';
 
 @Component({
   selector: 'app-header',
   imports: [RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  // providers:[TodosService]
 })
 export class HeaderComponent {
   title = 'My first ng app';
