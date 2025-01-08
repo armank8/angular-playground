f5175424eec55bb29a0c5312704a77ba1df1f2be
Arman-Kabir <armankabir8@gmail.com>
2025-01-08 09:54:24 +0600

    counter app

--- first-ng-app/src/app/home/home.component.ts
+++ first-ng-app/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { GreetingsComponent } from '../components/greetings/greetings.component';
+import { CounterComponent } from '../components/counter/counter.component';
 
 @Component({
   selector: 'app-home',
-  imports: [GreetingsComponent],
+  imports: [GreetingsComponent,CounterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
