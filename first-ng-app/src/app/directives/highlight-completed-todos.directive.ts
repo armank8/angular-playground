84f0d91633c0cc6b4d81c03661d597786af00d61
Arman-Kabir <armankabir8@gmail.com>
2025-01-13 13:30:59 +0600

    update todo

--- first-ng-app/src/app/directives/highlight-completed-todos.directive.ts
+++ first-ng-app/src/app/directives/highlight-completed-todos.directive.ts
@@ -4,19 +4,24 @@ import { Directive, input, effect, inject, ElementRef } from '@angular/core';
   selector: '[appHighlightCompletedTodos]',
 })
 export class HighlightCompletedTodosDirective {
-  icCompleted = input(false);
+  isCompleted = input(false);
   el = inject(ElementRef);
   constructor() {}
 
   stylesEffect = effect(() => {
-    if (this.icCompleted()) {
-      this.el.nativeElement.nativeElement.style.textDecoration = 'line-through';
-      this.el.nativeElement.nativeElement.style.backgroundColor = '#d3f9d8';
-      this.el.nativeElement.nativeElement.style.color = '#6c757d';
-    }else{
-      this.el.nativeElement.style.textDecoration='none';
-      this.el.nativeElement.style.textDecoration='#fff';
-      this.el.nativeElement.style.textDecoration='#000';
+    if (this.isCompleted()) {
+      this.el.nativeElement.style.textDecoration = 'line-through';
+      this.el.nativeElement.style.backgroundColor = '#d3f9d8';
+      this.el.nativeElement.style.color = '#6c757d';
+    } else {
+      this.el.nativeElement.style.textDecoration = 'none';
+      this.el.nativeElement.style.backgroundColor = '#fff';
+      this.el.nativeElement.style.color = '#000';
     }
   });
+
+  // cleanup effect when directive is destroyed
+  // ngOnDestroy() {
+  //   this.stylesEffect.destroy();
+  // }
 }
--- first-ng-app/src/app/todos/todos.component.ts
+++ first-ng-app/src/app/todos/todos.component.ts
@@ -14,6 +14,7 @@ import { TodoItemComponent } from '../components/todo-item/todo-item.component';
 export class TodosComponent implements OnInit {
   todoService = inject(TodosService);
   todoItems = signal<Array<Todo>>([]);
+  // console.log(todoItems);
 
   ngOnInit(): void {
     this.todoService
@@ -31,7 +32,17 @@ export class TodosComponent implements OnInit {
     // this.todoItems.set(this.todoService.todoItems);
   }
 
-  todoToggled(todoItem:Todo){
-    
+  UpdateTodoItem(todoItem: Todo) {
+    this.todoItems.update((todos) => {
+      return todos.map((todo) => {
+        if (todo.id === todoItem.id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      });
+    });
   }
 }
