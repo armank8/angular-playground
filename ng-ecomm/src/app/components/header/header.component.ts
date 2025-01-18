9a0300c95823216e746e81fcf0a90d7f90e49bb0
Arman-Kabir <armankabir8@gmail.com>
2025-01-18 16:54:12 +0600

    till button

--- ng-ecomm/src/app/components/header/header.component.ts
+++ ng-ecomm/src/app/components/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   template: `
-    <p class="bg-slate-100 px-4 py-3 shadow-md">
+    <p class="bg-slate-100 px-4 py-3 shadow-md flex justify-between">
       <span>My Store</span>
+      <app-primary-button></app-primary-button>
     </p>
   `,
   styles: ``,
